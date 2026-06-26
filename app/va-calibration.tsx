8414f087018ea73a004618735690a5afc382db78
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-06-26 21:58:29 +0200

    feat: implement VA calibration screen UI

--- app/va-calibration.tsx
+++ app/va-calibration.tsx
@@ -1,4 +1,17 @@
-export default function VACalibrationScreen() {
-  return null;
-}
+import { ScreenShell } from '@/components/ScreenShell';
+import { t } from '@/constants/translations';
+import { useAppContext } from '@/contexts/AppContext';
+import { useRequireConsent } from '@/hooks/useRequireConsent';
 
+export default function VaCalibrationScreen() {
+  useRequireConsent();
+  const { language } = useAppContext();
+
+  return (
+    <ScreenShell
+      title={t(language, 'vaCalibration')}
+      subtitle={t(language, 'vaCalibrationSubtitle')}
+      actions={[{ label: t(language, 'continue'), href: '/va-test', primary: true }]}
+    />
+  );
+}
