9fc4a07f6a1236145a3b9c112545915547ede062
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-06-26 21:58:29 +0200

    feat: implement welcome screen UI

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,4 +1,15 @@
-export default function WelcomeScreen() {
-  return null;
-}
+import { ScreenShell } from '@/components/ScreenShell';
+import { t } from '@/constants/translations';
+import { useAppContext } from '@/contexts/AppContext';
 
+export default function WelcomeScreen() {
+  const { language } = useAppContext();
+
+  return (
+    <ScreenShell
+      title={t(language, 'welcome')}
+      subtitle={t(language, 'welcomeSubtitle')}
+      actions={[{ label: t(language, 'continue'), href: '/language-select', primary: true }]}
+    />
+  );
+}
