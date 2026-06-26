0f0c112f31680d45653bb4044e85084c1a4d9842
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-06-26 21:58:29 +0200

    feat: implement VA test screen UI

--- app/va-test.tsx
+++ app/va-test.tsx
@@ -1,4 +1,17 @@
-export default function VATestScreen() {
-  return null;
-}
+import { ScreenShell } from '@/components/ScreenShell';
+import { t } from '@/constants/translations';
+import { useAppContext } from '@/contexts/AppContext';
+import { useRequireConsent } from '@/hooks/useRequireConsent';
 
+export default function VaTestScreen() {
+  useRequireConsent();
+  const { language } = useAppContext();
+
+  return (
+    <ScreenShell
+      title={t(language, 'vaTest')}
+      subtitle={t(language, 'vaTestSubtitle')}
+      actions={[{ label: t(language, 'viewResults'), href: '/va-result', primary: true }]}
+    />
+  );
+}
