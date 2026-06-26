ef474a7f769e6c0a482f0e880db33c5df0537006
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-06-26 21:58:29 +0200

    feat: implement history screen UI

--- app/history.tsx
+++ app/history.tsx
@@ -1,4 +1,17 @@
-export default function HistoryScreen() {
-  return null;
-}
+import { ScreenShell } from '@/components/ScreenShell';
+import { t } from '@/constants/translations';
+import { useAppContext } from '@/contexts/AppContext';
+import { useRequireConsent } from '@/hooks/useRequireConsent';
 
+export default function HistoryScreen() {
+  useRequireConsent();
+  const { language } = useAppContext();
+
+  return (
+    <ScreenShell
+      title={t(language, 'history')}
+      subtitle={t(language, 'historySubtitle')}
+      actions={[{ label: t(language, 'backHome'), href: '/home', primary: true }]}
+    />
+  );
+}
