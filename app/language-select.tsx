98aa17742951f3791db30b992a6bd1603a1c5700
Guillain BISIMWA <guillainbisimwa@hotmail.fr>
2026-06-26 21:58:29 +0200

    feat: implement language select screen UI

--- app/language-select.tsx
+++ app/language-select.tsx
@@ -1,4 +1,26 @@
-export default function LanguageSelectScreen() {
-  return null;
-}
+import { ScreenShell } from '@/components/ScreenShell';
+import { t } from '@/constants/translations';
+import { useAppContext } from '@/contexts/AppContext';
 
+export default function LanguageSelectScreen() {
+  const { language, setLanguage } = useAppContext();
+
+  return (
+    <ScreenShell
+      title={t(language, 'languageSelect')}
+      actions={[
+        {
+          label: t(language, 'english'),
+          selected: language === 'en',
+          onPress: () => setLanguage('en'),
+        },
+        {
+          label: t(language, 'french'),
+          selected: language === 'fr',
+          onPress: () => setLanguage('fr'),
+        },
+        { label: t(language, 'continue'), href: '/consent', primary: true },
+      ]}
+    />
+  );
+}
